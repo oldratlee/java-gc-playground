373704a5b6f5b18d2b5a7db4ab3a00a677921012
Jerry Lee <oldratlee@gmail.com>
2014-06-15 14:39:49 +0800

    first commit

---
+++ src/test/java/com/foo/JavaGcPlayGroundTest.java
@@ -0,0 +1,46 @@
+package com.foo;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+import java.util.Random;
+import java.util.WeakHashMap;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author ding.lid
+ */
+public class JavaGcPlayGroundTest {
+    private static final Logger logger = LoggerFactory.getLogger(JavaGcPlayGroundTest.class);
+
+    @Test
+    public void test_weakRefAndGc() throws Exception {
+        Thread thread = new Thread(new NewTask());
+        thread.start();
+
+        Map<Integer, byte[]> holder = new WeakHashMap<Integer, byte[]>();
+        for (int i = 0; ; ++i) {
+            if (i % 1000000 == 0)
+                logger.info("input count: {}m, total new byte array size: {}g", i / 1000000, i / 1000000);
+            holder.put(i, new byte[1024]);
+        }
+    }
+
+    static class NewTask implements Runnable {
+        @Override
+        public void run() {
+            Random random = new Random();
+            while (true) {
+                byte[] bytes = new byte[1024 * 1024];
+                logger.error("locate 1m byte array: {}", bytes.toString());
+                try {
+                    TimeUnit.SECONDS.sleep(random.nextInt(5));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
